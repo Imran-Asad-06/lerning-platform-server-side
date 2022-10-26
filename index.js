3b267b0c3cb4cb96c822639e6b676f13dbd05b24
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-10-26 14:44:22 +0600

    added course details

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
 const catagories = require('./data/catagories.json')
+const course = require('./data/course.json')
 
 app.get('/',(req,res)=>{
     res.send('online learning API running');
@@ -12,6 +16,12 @@ app.get('/',(req,res)=>{
 app.get('/course-catagories',(req,res)=>{
     res.send(catagories)
 })
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedcourse = course.find(n => n.id === id);
+    res.send(selectedcourse);
+});
 
 app.listen(port,()=>{
     console.log('online learning platform server running on port', port)
